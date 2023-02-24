ce52d26dda8969b96dad71ec8a65454401de7a4c
mabiiak <mabiane.polniak@gmail.com>
2023-02-24 13:49:02 -0300

    Criando repositório

---
+++ api.js
@@ -0,0 +1,34 @@
+export const relatorio_vendas = {
+    laranja: {
+        valor: "200,00",
+        dia: "terça",
+    },
+    peixe: {
+        valor: "50,00",
+        dia: "segunda",
+    },
+    conchas: {
+        valor: "5,00",
+        dia: "quarta",
+    },
+    grama: {
+        valor: "2,00",
+        dia: "quarta",
+    },
+    insetos: {
+        valor: "1000,00",
+        dia: "sexta",
+    },
+    fosseis: {
+        valor: "2000,00",
+        dia: "quinta",
+    },
+    roupas: {
+        valor: "500,00",
+        dia: "sabado",
+    },
+    decoracao: {
+        valor: "1200,00",
+        dia: "domingo",
+    },
+};
